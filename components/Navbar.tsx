59c38266ead00dcdcd1d69e30015e478da71cded
jon-jc <jonathancho.jc@gmail.com>
2024-03-18 15:32:13 -0700

    add Navbar
    Add components to Header and update package dependencies

---
+++ components/Navbar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
+
+const links = [
+  {
+    path: "/",
+    name: "Home",
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+  },
+];
+
+const Navbar = () => {
+  const path = usePathname();
+  return (
+    <div className="flex items-center gap-x-6">
+      {links.map((link, index) => (
+        <Link
+          href={link.path}
+          key={index}
+          className="relative hover:text-primary transition-all"
+        >
+          {link.path === path && (
+            <motion.span
+              initial={{ y: "-100%" }}
+              animate={{ y: 0 }}
+              transition={{ type: "tween" }}
+              layoutId="underline"
+              className="absolute left-0 top-full h-[3px] bg-primary w-full"
+            />
+          )}
+          {link.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Navbar;
